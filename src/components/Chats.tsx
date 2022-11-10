92991806fa3a41cc33f78e66db97e62e6006e8cd
Fogel Egor <fogel_92@bk.ru>
2022-11-10 21:26:12 +0700

    feat: render userChats

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -1,36 +1,44 @@
-import React from 'react';
-
+import React, {useContext, useEffect, useState} from 'react';
+import { doc, onSnapshot } from "firebase/firestore";
+import {db} from "../firebase";
+import {AuthContext} from "../context/AuthContext";
 const Chats = () => {
+
+  const [chats, setChats] = useState([]);
+  const {currentUser} = useContext(AuthContext);
+
+  useEffect(() => {
+   const getChats = () => {
+     const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+       // @ts-ignore
+       return setChats(doc.data());
+     });
+
+     return () => {
+       unsub();
+     };
+   };
+   currentUser.uid && getChats();
+  }, [currentUser.uid]);
+
+  console.log(Object.entries(chats));
+
   return (
     <div className='chats'>
-      <div className="userChat">
-        <img src="https://png.pngtree.com/png-vector/20190704/ourlarge/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt=""/>
-        <div className="userChatInfo">
-          <span>Егор</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img src="https://png.pngtree.com/png-vector/20190704/ourlarge/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt=""/>
-        <div className="userChatInfo">
-          <span>Егор</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img src="https://png.pngtree.com/png-vector/20190704/ourlarge/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt=""/>
-        <div className="userChatInfo">
-          <span>Егор</span>
-          <p>Hello</p>
-        </div>
-      </div>
-      <div className="userChat">
-        <img src="https://png.pngtree.com/png-vector/20190704/ourlarge/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt=""/>
-        <div className="userChatInfo">
-          <span>Егор</span>
-          <p>Hello</p>
-        </div>
-      </div>
+      {
+        Object.entries(chats)?.map(chat => (
+          <div className="userChat" key={chat[0]}>
+            {/* @ts-ignore*/}
+            <img src={chat[1].userInfo.photoURL} alt="Аватар пользователя"/>
+            <div className="userChatInfo">
+              {/* @ts-ignore*/}
+              <span>{chat[1].userInfo.displayName}</span>
+              {/* @ts-ignore*/}
+              <p>{chat[1].userInfo.text}</p>
+            </div>
+          </div>
+        ))
+      }
     </div>
   );
 };
