76632f95a7b739b54e442e4ad11c768c63375320
Fogel Egor <fogel_92@bk.ru>
2022-11-10 20:50:10 +0700

    feat: search user

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import {db} from "../firebase";
 
 const Search: React.FC = () => {
+  const [userName, setUserName] = useState('');
+  const [user, setUser] = useState({
+    displayName: '',
+    photoURL: '',
+  });
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, 'users'),
+      where("displayName", '==', userName)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        // @ts-ignore
+        return setUser(doc.data());
+      });
+    } catch (e) {
+      setErr(true);
+    }
+  }
+
+  const handleKey = (e: React.KeyboardEvent) => {
+    e.code === 'Enter' && handleSearch();
+  }
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='Найти пользователя'/>
+        <input type="text" placeholder='Найти пользователя'
+               onKeyDown={handleKey}
+               onChange={e => setUserName(e.target.value)}/>
       </div>
-      <div className="userChat">
-        <img src="https://png.pngtree.com/png-vector/20190704/ourlarge/pngtree-businessman-user-avatar-free-vector-png-image_1538405.jpg" alt=""/>
+      { err && <span>Пользователь не найден</span>}
+      { user.displayName &&
+        <div className="userChat">
+        <img
+          src={user.photoURL}
+          alt="Аватар пользователя"/>
         <div className="userChatInfo">
-          <span>Егор</span>
+          <span>{user.displayName}</span>
         </div>
-      </div>
+      </div> }
     </div>
   );
 };
